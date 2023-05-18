7b28518cbe6e64c1797cf513cfe594cb4150d5a8
Mathankumar <109581628+mathankumarsmech97@users.noreply.github.com>
2023-05-18 09:58:44 +0530

    Add files via upload

---
+++ ADO.Net/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Configuration;
+
+namespace ADO.Net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+
+                SqlCommand command = new SqlCommand("SELECT * FROM [dbo].[CustomerAddress]", con);
+                con.Open();
+                SqlDataReader readerreader = command.ExecuteReader();
+                GridView1.DataSource = readerreader;
+                GridView1.DataBind();
+                con.Close();
+            }
+
+
+
+
+
+        }
+    }
+}
